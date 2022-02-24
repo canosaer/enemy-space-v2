cadc67cdaa75e6e4e22b4c55efc98567aebdba9e
canosaer <67963828+canosaer@users.noreply.github.com>
2022-02-24 14:58:45 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import './styles/main.scss'
+
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from 'react-router-dom'
+
+// import Home from './views/Home'
+// import Shop from './views/Shop'
+// import Cart from './views/Cart'
+
+
+function App() {
+
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          {/* <Route path="/" element={<Home />} />
+          <Route path="/shop" element={<Shop />} />
+          <Route path="/cart" element={<Cart />} /> */}
+        </Routes>
+      </BrowserRouter>
+    </>
+  )
+}
+
+export default App
---
+++ src/store/reducer.js
@@ -0,0 +1,18 @@
+const reducer = (state, action) => {
+    switch(action.type) {
+        // case 'ADD_ITEM':
+        //     return {
+        //         ...state,
+        //         cart: action.payload
+        //     }
+        // case 'REMOVE_ITEM':
+        //     return{
+        //         ...state,
+        //         cart: action.payload
+        //     }
+        default: 
+            return state
+    }
+}
+
+export default reducer
