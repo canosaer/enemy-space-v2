010e1f211b2fed5900fa5429951e8cf03d865d1d
canosaer <canosaer@gmail.com>
2022-02-25 17:34:38 -0500

    c
    more

--- src/views/Title.js
+++ src/views/Title.js
@@ -11,7 +11,7 @@ export default function Title() {
             <Link to="/play" className="title-screen__link">
                 <h1 className="title-screen__title">Dark Moon</h1>
                 <h2 className="title-screen__subtitle">Enemy Space</h2>
-                
+                <img src="../img/title.png" alt="title image" />
             </Link>
         </main>
     )
