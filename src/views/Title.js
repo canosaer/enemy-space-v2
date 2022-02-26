1b09f5ae13fdb651d7ff247a2c5062d355b8dd9c
canosaer <67963828+canosaer@users.noreply.github.com>
2022-02-26 12:30:33 -0500

    title screen

--- src/views/Title.js
+++ src/views/Title.js
@@ -1,7 +1,9 @@
 import React from 'react'
 
-import { Link } from 'react-router-dom';
+import { Link } from 'react-router-dom'
 // import Header from '../components/Header'
+
+import titleImage from '../img/title.png'
 
 
 export default function Title() {
@@ -9,9 +11,10 @@ export default function Title() {
     return(
         <main className="title-screen">
             <Link to="/play" className="title-screen__link">
-                <h1 className="title-screen__title">Dark Moon</h1>
-                <h2 className="title-screen__subtitle">Enemy Space</h2>
-                <img src="../img/title.png" alt="title image" />
+                <h1 className="title-screen__text title-screen__text_title">Dark Moon</h1>
+                <h2 className="title-screen__text title-screen__text_subtitle">Enemy Space</h2>
+                <img className="title-screen__image" src={titleImage} alt="title image" />
+                <figure className="title-screen__image-background"></figure>
             </Link>
         </main>
     )
