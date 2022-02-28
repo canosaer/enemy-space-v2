c353a6e73b11c1ebdf0c400ce9da3fb6ebeab26d
canosaer <67963828+canosaer@users.noreply.github.com>
2022-02-27 21:03:13 -0500

    styles

--- src/components/EncounterScene.js
+++ src/components/EncounterScene.js
@@ -1,14 +1,26 @@
+import monster from '../img/monster.png'
+
 export default function EncounterScene() {
     
     return(
         <section className="scene">
             <h2 className="scene__heading">Starspawn Attack!</h2>
-            <p className="scene__event">A massive starspawn bears down on your ship, trying to swallow it whole.</p>
-            <p className="scene__choices">You can try to fend it off by firing your weapons, or you can take evasive maneuvers and try to outrun it...</p>
-            <button className="scene__button scene__button_a">Fire Your Weapons</button>
-            <button className="scene__button scene__button_b">Take Evasive Manuevers</button>
-            <p className="scene__resolution scene__resolution_a_success">You unleash a volley of missiles at the starspawn and manage to fire them directly into its gaping maw. The starspawn reels back in pain and anger and retreats, allowing you to continue on your way. </p>
-            <button className="scene__button scene__continue-button">Continue</button>
+            <figure className="scene__image-slot">
+                <img className="scene__image" src={monster} alt="space monster" />
+            </figure>      
+            <div className="scene__text">
+                <p className="scene__event">A massive starspawn bears down on your ship, trying to swallow it whole!</p>
+                <p className="scene__choices">You can try to fend it off by firing your weapons, or you can take evasive maneuvers and try to outrun it...</p>
+                <div className="scene__button-row">
+                    <button className="scene__button scene__button_action scene__button_a">Fire Your Weapons</button>
+                    <button className="scene__button scene__button_action scene__button_b">Take Evasive Manuevers</button>
+                </div>
+                <div className="resolution">
+                    <p className="resolution__text scene__resolution_a_success">You unleash a volley of missiles directly into the starspawn's gaping maw. It reels in pain and slinks away, allowing you to continue on your way. </p>
+                    <button className="scene__button scene__button_continue">Continue</button>
+                </div>
+            </div>
+            
         </section>
     )
 }
--- src/components/Player.js
+++ src/components/Player.js
@@ -4,11 +4,16 @@ export default function Player() {
     
     return(
         <section className="player">
-            <img className="player__image" src={playerShip} alt="player ship" />
-            <p className="player__weapons">Weapons: <span className="player__weapons-level">Operational</span></p>
-            <p className="player__life-support">Life Support: <span className="player__life-support-level">Operational</span></p>
-            <p className="player__power">Power: <span className="player__power-level">Operational</span></p>
-            <p className="player__power">Engines: <span className="player__engines-level">Operational</span></p>
+            <figure className="player__background">
+                <img className="player__image" src={playerShip} alt="player ship" />
+            </figure>
+            <div className="player__stats">
+                <p className="player__weapons">Weapons: <span className="player__weapons-level">Operational</span></p>
+                <p className="player__life-support">Life Support: <span className="player__life-support-level">Operational</span></p>
+                <p className="player__power">Power: <span className="player__power-level">Operational</span></p>
+                <p className="player__power">Engines: <span className="player__engines-level">Operational</span></p>
+                {/* Levels: Operational, Damaged, Impaired, Critical */}
+            </div>
         </section>
     )
 }
