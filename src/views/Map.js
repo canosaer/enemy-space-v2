c1ac37751b575f669014e74b19de4f49e3459c8a
canosaer <67963828+canosaer@users.noreply.github.com>
2022-03-12 14:55:37 -0500

    deleted extra comments

--- src/views/Map.js
+++ src/views/Map.js
@@ -1,17 +1,11 @@
 import React, {useContext} from 'react'
 import {Context} from '../store/store'
-// import ShortUniqueId from 'short-unique-id'
-// import { sortByKey, addItemToCart } from '../utilities'
 import MapHeader from '../components/MapHeader'
 import MapDisplay from '../components/MapDisplay'
 
 export default function Map() {
     const [state, dispatch] = useContext(Context)
-   
-    // const uid = new ShortUniqueId({length:10})
     
-
-
     return(
         <main className="map">
             <figure className="map__container">
